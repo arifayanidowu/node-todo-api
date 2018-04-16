323ad4b9d3ec0da62a4ead83408384c55a177d74
stizzle123 <arifayanidowu2@yahoo.com>
2018-04-16 19:39:52 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb')
 
 let { mongoose } = require('./db/mongoose');
 let { Todo } = require('./models/todo');
@@ -29,6 +30,46 @@ app.get('/todos', (req, res) => {
         res.status(400).send(err);
     })
 });
+
+app.get('/todos/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(400).send();
+    };
+
+    Todo.findById(id).then((todos) => {
+        if (!todos) {
+            return res.status(404).send({})
+        } else {
+            res.status(200).send({ todos });
+        }
+    }, err => {
+        res.status(400).send();
+    });
+});
+
+
+
+app.get('/users/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    };
+
+    User.findById(id).then((user) => {
+        if (!user) {
+            return res.status(404).send({});
+        } else {
+            res.status(200).send({ user });
+        }
+    }, err => {
+        res.status(400).send();
+    });
+});
+
 
 app.listen(3000, () => {
     console.log(`Server started on port 3000`);
